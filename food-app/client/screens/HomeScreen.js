596e9894bee7fcad3fd12eeab7ed2474c422be94
siddkalani <siddharthkalani6@gmail.com>
2024-07-31 01:21:02 +0530

    setup

---
+++ food-app/client/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+    </SafeAreaView>
+  )
+}
